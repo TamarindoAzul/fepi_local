daecb602f9bfb5f053e98f876475926724e421e2
Tamarindo Azul <aguadianamatinez@gmail.com>
2024-12-15 22:51:55 -0600

    ACTUMANIFEST

--- lib/constansts/app_colors.dart
+++ lib/constansts/app_colors.dart
@@ -4,9 +4,5 @@ class AppColors {
   static const Color color1 = Color.fromRGBO(255, 255, 255, 1); 
   static const Color color2 = Color.fromRGBO(0, 0, 0, 1); 
   static const Color color3 = Color.fromRGBO(144, 12, 63, 1);
-<<<<<<< HEAD
   static const Color color4 = Color.fromRGBO(253, 250, 206, 1);
-=======
-  static const Color color4 = Color.fromRGBO(253, 250, 206, 1.0);
->>>>>>> f59828d96056b3c701320853e2d341b91ffe28dd
 }
